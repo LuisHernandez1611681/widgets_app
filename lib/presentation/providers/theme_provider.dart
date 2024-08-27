b41d257908f2b6ebdcf66507e1ff735d7c4e9b75
luisHdz <luishernandez1611681@gmail.com>
2024-08-26 20:56:59 -0600

    Uso de StateNotifierProvider

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,6 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
+/*
+  Porvider: Para valores inmutables
+  StateProvider: Mantener una pieza de estado
+  StateNotifierProvider: 
+*/
+
 // Un simple boolean
 final isDarkModeProvider = StateProvider<bool>((ref) => false);
 
@@ -9,3 +15,18 @@ final colorListProvider = Provider((ref) => colorList);
 
 // Un simple Int
 final selectedColorProvider = StateProvider((ref) => 0);
+
+// Un objeto de tipo AppTheme (custom)
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppTheme>((ref) {
+  return ThemeNotifier();
+});
+
+//  Controller o Notifier
+// Se va encargar de mantener un estado en particular
+class ThemeNotifier extends StateNotifier<AppTheme> {
+  // Se crea una nueva instancia de AppTheme
+  // se utiliza con state para poder acceder a sus propiedades
+  // ThemeNotifier(super._state);
+  ThemeNotifier(): super(AppTheme());
+
+}
