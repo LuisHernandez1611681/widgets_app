e0497734a3fb8b3a068a4e454565fa7735d1ad5d
luisHdz <luishernandez1611681@gmail.com>
2024-08-26 19:48:38 -0600

    Creamos un estado de provider con riverpod

---
+++ lib/presentation/providers/counter_provider.dart
@@ -0,0 +1,4 @@
+// Provedor de un estado
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final counterProvider = StateProvider((ref) => 5);
--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter-screen';
 
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // watch -> que este pendiente de nuestro counterProvider
+    final int clickCounter = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
@@ -14,7 +19,7 @@ class CounterScreen extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         child: Text(
-          'Valor: 10',
+          'Valor: $clickCounter',
           style: Theme.of(context).textTheme.titleLarge
         ),
       ),
