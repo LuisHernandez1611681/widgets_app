c5346b4829c609d0f17ffca0754fb5a2308413c0
luisHdz <luishernandez1611681@gmail.com>
2024-08-26 20:03:25 -0600

    Uso de StateProvider y la forma de poder actualizarlo en metodos

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const String name = 'counter-screen';
@@ -10,11 +11,23 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // watch -> que este pendiente de nuestro counterProvider
+    // No usarlo en metodos
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined 
+            ),
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier).update((state) => !state);
+            }
+          )
+        ],
       ),
       body: Container(
         alignment: Alignment.center,
@@ -25,7 +38,11 @@ class CounterScreen extends ConsumerWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          
+          // de esta forma podemos hacer uso de la lectura
+          ref.read(counterProvider.notifier).state++;
+
+          // Esta es otra forma de poder hacerlo
+          // ref.read(counterProvider.notifier).update((state) => state + 1);
         },
         child: const Icon(Icons.add),
       ),
